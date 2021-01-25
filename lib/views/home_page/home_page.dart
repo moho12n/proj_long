d7c3fb81d550f7021251bb3530569d8389739e54
Mehdi <Aknoun.mehdi11@gmail.com>
2021-01-25 12:22:36 +0100

    Added dark mode to maps

--- lib/views/home_page/home_page.dart
+++ lib/views/home_page/home_page.dart
@@ -3,16 +3,23 @@ import 'package:proj_long/views/home_page/widgets/circular_menu.dart';
 import 'package:proj_long/views/home_page/widgets/circular_menu_item.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:proj_long/views/tools/colors.dart';
-
+import 'package:flutter/services.dart' show rootBundle;
 import 'dart:async';
 
 //Global Variables
 const LatLng _center = const LatLng(48.8566, 2.3522);
 Set<Marker> markers = {};
+String _darkMapStyle;
+String _lightMapStyle;
 
 class HomePage extends StatefulWidget {
   @override
   _MyAppState createState() => _MyAppState();
+}
+
+Future _loadMapStyles() async {
+  _darkMapStyle = await rootBundle.loadString('assets/map_styles/dark.json');
+  _lightMapStyle = await rootBundle.loadString('assets/map_styles/light.json');
 }
 
 class _MyAppState extends State<HomePage> {
@@ -24,6 +31,18 @@ class _MyAppState extends State<HomePage> {
   void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
     _controller.complete(controller);
+    _setMapStyle();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _loadMapStyles();
+  }
+
+  Future _setMapStyle() async {
+    final controller = await _controller.future;
+    controller.setMapStyle(_darkMapStyle);
   }
 
   @override
@@ -38,7 +57,7 @@ class _MyAppState extends State<HomePage> {
 
                     myLocationEnabled: true,
                     myLocationButtonEnabled: false,
-                    zoomControlsEnabled: false,
+                    zoomControlsEnabled: false, // not sure if we should keep it
                     onMapCreated: _onMapCreated,
                     initialCameraPosition: CameraPosition(
                       target: _center,
