069697d1663e4d7509271e1972e6a3442495d839
Mehdi <Aknoun.mehdi11@gmail.com>
2021-02-02 11:41:38 +0100

    Added profile and setting page

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:proj_long/views/home_page/home_page.dart';
 import 'package:proj_long/views/preferences/preferences_page.dart';
 import 'package:proj_long/views/tools/colors.dart';
 import 'package:proj_long/views/tools/dimensions.dart';
+import 'package:proj_long/views/profile_page/edit_profile.dart';
 import 'package:get/get.dart';
 import 'views/create_room/create_room_screen.dart';
 
@@ -87,7 +88,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
           backgroundColor: ThemeColors.backgroundColor,
-          body: CreateRoomScreen()),
+          body: EditProfilePage()),
     );
   }
 }
